fff524a2ac748cf78210ccfe4fb0d933f91717c7
MuhiddinK <muti67200@gmail.com>
2023-03-15 14:31:02 +0100

    variables

---
+++ 1-variable.js
@@ -0,0 +1,28 @@
+console.log('hello world');
+
+var quantity;
+quantity = 5;
+console.log(quantity);
+
+var i = 100;
+i = 10;
+console.log(i);
+
+/**
+ *  Primitive Data Types: numbers, string, boolean, null, undefined (variable does not have a value yet) * 
+ */
+
+var a;
+console.log(a); // undefined: because we did not declare any value!
+
+console.log(typeof i);
+
+/**
+ *  Prompt operator works like scanner in Java, so we can have user input 
+ */
+
+
+// Scanner in JS is prompt operator
+var name = prompt('Enter your name: ');
+console.log(name);
+
