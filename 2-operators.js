02dfdfe80f87f240a5c11f99cc8f29cdfd989d5d
MuhiddinK <muti67200@gmail.com>
2023-03-15 14:30:13 +0100

    variables and operators JS

---
+++ 2-operators.js
@@ -0,0 +1,19 @@
+// Arithmetic
+console.log(3+5*4-6);
+
+// Assignment
+var x = 5;
+x+=5;
+console.log(x);
+
+// Comparison
+var one = 1;
+var one_again = 1;
+var one_string = '1';
+
+console.log(one == one_again); // true
+console.log(one === one_again); // true
+console.log(one == one_string); // true => checks the same value rgardles of the types
+console.log(one === one_string); // false => checks also the types
+
+// Logical
